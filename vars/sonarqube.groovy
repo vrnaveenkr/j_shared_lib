9368a572650812a793b400465b7be23ab8ad485e
vrnaveenkr <vrnaveenkr@ub21-01.localdomain>
2022-01-20 21:27:39 +0530

    mylib

--- vars/sonarqube.groovy
+++ vars/sonarqube.groovy
@@ -1,25 +1,8 @@
 def call(){
     echo "${scm.userRemoteConfigs[0].url}"
-    pipeline {
-    agent any
-    stages {
-        stage('Clone sources') {
-            steps {
-                git url: 'https://github.com/vrnaveenkr/sonarqube-jacoco-code-coverage.git'
-            }
-        }
-        stage('SonarQube analysis') {
-            steps {
-                withSonarQubeEnv('sonarqube') {
-                    sh "./gradlew sonarqube"
-                }
-            }
-        }
-        stage("Quality gate") {
-            steps {
-                waitForQualityGate abortPipeline: true
-            }
-        }
-    }
+  
+    withSonarQubeEnv('sonarqube') {
+        sh "./gradlew sonarqube"
+              
 }
 }
